540ff85b323db7a5e33b89cde427dbc5a967a285
selinozluk <selinozluk36@gmail.com>
2025-07-14 00:09:10 +0300

    fix and update

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,8 +1,8 @@
 ﻿using Inheritance.Models;
 
-class Program
+public class Program
 {
-    static void Main (string[] args)
+   public static void Main (string[] args)
     {
 
         // Öğretmen nesnesi
